7e9dbe3e65fa51a4bcb184d8eff162f048393b3e
adeafblindman <adeafblindman@gmail.com>
2024-05-22 13:59:41 -0500

    Update z4.js

--- scripts/z4.js
+++ scripts/z4.js
@@ -110,7 +110,7 @@ $(window).on('load', function() {
       var iconSize = point['Custom Size'];
       var size = (iconSize.indexOf('x') > 0)
        ? [parseInt(iconSize.split('x')[0]), parseInt(iconSize.split('x')[1])]
-        : [60, 60]; // zoom level 4
+        : [100, 100]; // zoom level 4
 // line 113 might set icon size if there is none defined in GSheets
 
       
