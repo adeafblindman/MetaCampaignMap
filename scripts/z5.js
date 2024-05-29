0de16b7b224f48776381842bfa98108980c82465
adeafblindman <adeafblindman@gmail.com>
2024-05-28 19:22:09 -0500

    Update z5.js

--- scripts/z5.js
+++ scripts/z5.js
@@ -170,7 +170,7 @@ $(window).on('load', function() {
         : getSetting('_pointsLegendPos');
 
       var pointsLegend = L.control.layers(null, layers, {
-        collapsed: false,
+        collapsed: true,
         position: pos,
       });
 
